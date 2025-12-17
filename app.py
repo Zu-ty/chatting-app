49dac3d04461d2cdd21cdf84dc29b11c066d6928
Zu-ty <Onyerichizuterem@gmail.com>
2025-12-16 23:48:10 -0800

    Set Flask-SocketIO debug mode to False
    Changed debug mode to False for production.

--- app.py
+++ app.py
@@ -14,4 +14,5 @@ def handle_send_message(data):
     emit('receive_message',data, broadcast=True)
 
 if __name__ == '__main__':
-    socketio.run(app, debug=True)
+
+    socketio.run(app, debug=False)
